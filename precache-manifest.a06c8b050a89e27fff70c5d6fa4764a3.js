f91126f826c6e7b4043b16f13c1219e1cdf754b2
Michael Gates <michaelgatesdev@gmail.com>
2020-04-27 16:00:13 -0400

    Updates

---
+++ precache-manifest.a06c8b050a89e27fff70c5d6fa4764a3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79046ebb16776483fbd8e4492431ec99",
+    "url": "/index.html"
+  },
+  {
+    "revision": "62d6bfd7049f95062765",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "1ceaadd63dfb2de5279e",
+    "url": "/static/js/2.70237da7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.70237da7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62d6bfd7049f95062765",
+    "url": "/static/js/main.c177b74f.chunk.js"
+  },
+  {
+    "revision": "0021f0f90b22fdb42d71",
+    "url": "/static/js/runtime-main.e7cad34d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
